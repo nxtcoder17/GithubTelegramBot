ac49cc25db340e67d797060479fd0764acfbfd3c
nxtcoder17 <nxtcoder17@gmail.com>
2020-06-14 22:28:41 +0530

    run.py changes

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import app
 import os
 
-app.run(host="localhost" if 'HOST' in os.environ and os.environ['HOST'] == 'Baby' else '0.0.0.0',
-        port=9999,
-        debug=True)
+if __name__ == '__main__':
+    app.run(host="localhost" if 'HOST' in os.environ and os.environ['HOST'] == 'Baby' else '0.0.0.0',
+            port=9999,
+            debug=True)
