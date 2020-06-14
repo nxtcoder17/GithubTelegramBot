69e583ef22ef1ee5f80bafa08ae39f4ebb41f349
nxtcoder17 <nxtcoder17@gmail.com>
2020-06-15 01:12:18 +0530

    Telegram Bot in progress

--- app/routes.py
+++ app/routes.py
@@ -27,5 +27,6 @@ def index():
 def github_event():
     if request.headers['content-type'] == 'application/json':
         pprint(request.json)
+        bot.send_message('847119416', request.json)
         return request.json
     return json.dumps({"msg": "No Response"})
