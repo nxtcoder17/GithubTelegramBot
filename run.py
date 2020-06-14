1f974edf666599fd28409fdda3badb378a4cee33
nxtcoder17 <nxtcoder17@gmail.com>
2020-06-14 22:23:33 +0530

    run.py file changes

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import app
+import os
 
-app.run(host="localhost",
-        port=9999,
+app.run(host="localhost" if os.environ['HOST'] == 'Baby' else '0.0.0.0',
+        port=9999 if os.environ['HOST'] == 'Baby' else 80,
         debug=True)
