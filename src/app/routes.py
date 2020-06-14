f828f857747837111d0cba34dbfd0148e6ed58f9
nxtcoder17 <nxtcoder17@gmail.com>
2020-06-14 20:43:57 +0530

    Init Version

---
+++ src/app/routes.py
@@ -0,0 +1,15 @@
+from app import app
+
+
+@app.route('/')
+def index():
+    print("Flask, Web server is UP")
+    return "Flask is running"
+
+
+@app.route('/github', methods=['POST'])
+def github_event(req):
+    if req.headers['content-type'] == 'application/json':
+        print(req.json)
+        return req.json
+    return False
---
+++ src/run.py
@@ -0,0 +1,5 @@
+from app import app
+
+app.run(host = "localhost",
+        port = 9999,
+        debug = True)
